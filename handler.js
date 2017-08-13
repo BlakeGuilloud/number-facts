47c2004c3763a88f8d6347466be41dbdb88be308
Blake Guilloud <blake.guilloud@gmail.com>
2017-08-13 18:25:48 -0400

    Some cleanup. using a twilio lambda func instead of rolling one in the service itself.

--- handler.js
+++ handler.js
@@ -1,5 +1,8 @@
 'use strict';
 
+const AWS = require('aws-sdk');
+const lambda = new AWS.Lambda({ region: 'us-east-1' });
+
 const sendMessage = require('./lib/message');
 const getFact = require('./lib/fact');
 const { handleError, handleSuccess } = require('./lib/helpers');
